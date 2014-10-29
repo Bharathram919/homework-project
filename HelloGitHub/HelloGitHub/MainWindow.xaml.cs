0730172cf86c2aa7a1e080f8fa2131c17d5f314c
Bharathram Attiyannan <bharathram@gmail.com>
2014-10-30 00:12:34 +0530

    Created First Solution

---
+++ HelloGitHub/HelloGitHub/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloGitHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();//this
+        }
+    }
+}
